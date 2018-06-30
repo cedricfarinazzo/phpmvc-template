48dcfa6bd362b1b68928e81379921c91bb16bdaf
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-06-30 14:41:49 +0200

    adding materialyzecss

--- _shared/layout.php
+++ _shared/layout.php
@@ -6,6 +6,7 @@
     <meta name="description" content="<?= PROJECT_DESCRIPTION; ?>" />
     <title><?= isset($view_title) ? $view_title.' | ' : ''; ?><?= PROJECT_NAME; ?></title>
     <link href="/assets/css/style.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/css/materialize.min.css">
     <link rel="icon" type="image/x-icon" href="/assets/medias/images/favicon.ico" />
 </head>
 <body>
@@ -23,6 +24,7 @@
         <?php require 'partial/foot.php'; ?>
     </footer>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-rc.2/js/materialize.min.js"></script>
     <script src="/assets/js/script.js" ></script>
 </body>
 </html>
